b6f8b893e7d8ce9d7af2363e4fb1067e95def9d2
jhojan290 <davio13bar@gmail.com>
2025-12-07 14:52:49 -0500

    new changes

--- resources/views/livewire/user/elements/navbar.blade.php
+++ resources/views/livewire/user/elements/navbar.blade.php
@@ -9,7 +9,7 @@
             <div class="flex items-center gap-8">
                 <a href="/inicio" class="flex items-center gap-4 text-white hover:opacity-90 transition-opacity group">
                     <img 
-                        src="{{ asset('img/LicUp.png') }}" 
+                        src="{{ asset('img/licUp.png') }}" 
                         alt="Logo LicUp" 
                         class="h-12 w-auto object-contain group-hover:scale-105 transition-transform duration-300"
                     >
